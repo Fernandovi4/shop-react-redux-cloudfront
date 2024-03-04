138671fac0939a2746576ab696a6eee247d8e55d
Fernandovi4 <79011727+Fernandovi4@users.noreply.github.com>
2024-03-04 18:53:14 +0000

    feat: added Component

---
+++ src/components/MainLayout/components/Component.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import { Component } from './Component'
+
+describe('<New Component/>', () => {
+    it('renders title', () => {
+        render(<Component />)
+        expect(screen.getByText('New Component')).toBeInTheDocument()
+    })
+})
